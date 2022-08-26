90c8c9c4dc6518edaec09900e43919f1aa9b6ba7
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2022-08-26 19:09:50 -0300

    resolução da questão 1, amém

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-  // começando o projeto!
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) return [];
+  const teste = species.filter((element) => ids.includes(element.id));
+  return teste;
 }
+
+// usamos o includes para verificar se o parâmetro passado (o array ids) inclui a id do elemento que está sendo iterado
 
 module.exports = getSpeciesByIds;
