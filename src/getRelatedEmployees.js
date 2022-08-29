f81034a0f0d5a768146256431f2a329e0b8c689b
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2022-08-29 13:42:50 -0300

    resolução do requisito 4! :D

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,26 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+// Precisamos de dois loops: um para checar se algum dos elementos da array inclui o id;
+// Depois repetir isso para todos os objetos da array employees
 function isManager(id) {
-  // seu código aqui
+  return employees.some((element) => element.managers.includes(id));
 }
 
+console.log(isManager('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
+// employees.some((employee) => employee.managers.some((manag) => manag === id));
+
+// O que fazer: percorrer o array employees, se em algum objeto managers incluir a ID, pegar o nome e sobrenome da pessoa
+
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const isSheHeManager = isManager(managerId);
+  if (isSheHeManager === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const getEmployees = (id) => employees.filter((element) => element.managers.includes(id)); // 1. Pegar o objeto correspondente à cada funcionario supervisado
+  return getEmployees(managerId).map((employee) => `${employee.firstName} ${employee.lastName}`); // 2. Em cada objeto, pegar nome e sobrenome do funcionário;
 }
+
+console.log(getRelatedEmployees('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
 
 module.exports = { isManager, getRelatedEmployees };
