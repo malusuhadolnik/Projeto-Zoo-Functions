519480c91aa52f7f2114e8fd3f6763934751718c
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2022-08-31 10:23:13 -0300

    resolução do requisito 9! :)

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,105 @@
 const data = require('../data/zoo_data');
 
+// inicio: para compor o objeto fullSchedule
+const horas = Object.entries(data.hours);
+
+const opn = (weekday) => {
+  const getDay = horas.filter((element) => element.includes(weekday));
+  return Object.values((getDay[0])[1])[0];
+};
+
+const close = (weekday) => {
+  const getDay = horas.filter((element) => element.includes(weekday));// pega a array correspondente à weekday
+  const getOpenClose = (getDay[0])[1]; // acessa o objeto dentro da array
+  return Object.values(getOpenClose)[1]; // pega o valor correspondete à open
+};
+
+const getAnimalsByDay = (weekday) => {
+  const teste = data.species.filter((element) => element.availability.includes(weekday));
+  return teste.map((element) => element.name);
+};
+
+const fullSchedule = {
+  Tuesday: {
+    officeHour: `Open from ${opn('Tuesday')}am until ${close('Tuesday')}pm`,
+    exhibition: getAnimalsByDay('Tuesday'),
+  },
+  Wednesday: {
+    officeHour: `Open from ${opn('Wednesday')}am until ${close('Wednesday')}pm`,
+    exhibition: getAnimalsByDay('Wednesday'),
+  },
+  Thursday: {
+    officeHour: `Open from ${opn('Thursday')}am until ${close('Thursday')}pm`,
+    exhibition: getAnimalsByDay('Thursday'),
+  },
+  Friday: {
+    officeHour: `Open from ${opn('Friday')}am until ${close('Friday')}pm`,
+    exhibition: getAnimalsByDay('Friday'),
+  },
+  Saturday: {
+    officeHour: `Open from ${opn('Saturday')}am until ${close('Saturday')}pm`,
+    exhibition: getAnimalsByDay('Saturday'),
+  },
+  Sunday: {
+    officeHour: `Open from ${opn('Sunday')}am until ${close('Sunday')}pm`,
+    exhibition: getAnimalsByDay('Sunday'),
+  },
+  Monday: {
+    officeHour: 'CLOSED',
+    exhibition: 'The zoo will be closed!',
+  },
+};
+console.log(fullSchedule);
+// fim da composição do objeto fullSchedule
+
+const getAnimalSchedule = (scheduleTarget) => {
+  const filterAnimal = data.species.filter((element) => element.name === scheduleTarget);
+  return filterAnimal[0].availability;
+};
+// console.log(getAnimalSchedule('elephants'));
+
+const dailySchedule = (scheduleTarget) => {
+  if (scheduleTarget === 'Monday') {
+    return { Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } };
+  }
+  const key = scheduleTarget;
+  const object = {};
+  object[key] = { officeHour: `Open from ${opn(scheduleTarget)}am until ${close(scheduleTarget)}pm`,
+    exhibition: getAnimalsByDay(scheduleTarget) };
+  return object;
+};
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (scheduleTarget === undefined) {
+    return fullSchedule;
+  }
+  if (data.species.map((element) => element.name).includes(scheduleTarget)) {
+    return getAnimalSchedule(scheduleTarget);
+  }
+  if (Object.keys(data.hours).includes(scheduleTarget)) {
+    return dailySchedule(scheduleTarget);
+  }
+  return fullSchedule;
 }
 
+// console.log(getSchedule()); // Funciona
+// console.log(getSchedule('lions'));
+// console.log(getSchedule('giraffes'));
+// console.log(getSchedule('Monday'));
+// console.log(getSchedule('Tuesday'));
+// console.log(getSchedule('Freja'));
 module.exports = getSchedule;
+
+// Retorne um array com os dias da semana em que um animal está disponível para visitação caso o parâmetro da função seja um animal. Por exemplo: [ 'Tuesday', 'Thursday', 'Saturday', 'Sunday' ];
+// A função, caso receba um único dia por parâmetro, deverá retornar os horários para aquele dia e quais animais estarão disponíveis.
+
+// Retorne todos os horários disponíveis para cada dia da semana caso a função:
+// - não receba parâmetro;
+// - o parâmetro passado para a função não seja um animal ou um dia;
+
+// Para isso:
+// Crie um objeto e adicione todos os dias da semana como chave;
+// Os valores de cada dia da semana deve ser um objeto, possuindo as chaves officeHour e exhibition:
+
+// officeHour deve possuir o texto com o horário que o zoológico abre e fecha naquele dia da semana;
+// exhibition deve possuir um array com o nome de todos os animais disponíveis para visitação naquele dia da semana.
